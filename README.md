b5f6fe38a03fdc7712fffd57d39d39cfb8e6b6d9
QHpix <xd@nowhere.com>
2019-06-12 15:10:12 +0100

    socket support

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pwnsploit
 This tool requires you to have `pwntools` installed
+
+## Basic syntax
+To check for both buffer-overflow and format string attack just run pwnsploit and supplying the filename as an argument: `./pwnsploit.py file`
+
+To use sockets add the arguments `-H HOST` and `-p PORT`
--- pwnsploit.py
+++ pwnsploit.py
@@ -4,16 +4,20 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t','--timeout',type=int)
-parser.add_argument('-b','--bof',action='store_true')
-parser.add_argument('-f','--format',action='store_true')
-parser.add_argument('file')
+parser.add_argument('-t', '--timeout', type=int)
+parser.add_argument('-b', '--bof', action='store_true')
+parser.add_argument('--format', action='store_true')
+parser.add_argument('-H', '--host')
+parser.add_argument('-p', '--port', type=int)
+parser.add_argument('-f', '--file')
 
-def check_bof(file, timeout=1,payload_size=1000,tries=1):
+def check_bof(file, timeout=1, payload_size=1000, tries=1):
     
     payload = cyclic(payload_size) #generate a pattern of `payload_size`
-
-    p = process(file)
+    if host and port:
+        p = remote(host, port)
+    else:
+        p = process(file)
     #receive until there is no more output
     if p.can_recv(timeout):
         while True:
@@ -27,7 +31,7 @@ def check_bof(file, timeout=1,payload_size=1000,tries=1):
     try:
         core = Coredump('./core')
         if '64' in core.arch:
-            rsp = core.read(core.rsp,4)
+            rsp = core.read(core.rsp, 4)
             assert rsp in payload
             length = cyclic_find(rsp)
         else:
@@ -45,13 +49,16 @@ def check_bof(file, timeout=1,payload_size=1000,tries=1):
         log.info('Executable is NOT vulnerable to buffer overflow')
 
     else:
-        check_bof(file,timeout,payload_size=(payload_size+100),tries=(tries+1))
+        check_bof(file, timeout, payload_size=(payload_size+100), tries=(tries+1))
 
-def check_format(file, timeout=1):
+def check_format(file, timeout=1, host=None, port=None):
     
     payload = 'AAAA.'+'%x.'*15
 
-    p = process(file)
+    if host and port:
+        p = remote(host, port)
+    else:
+        p = process(file)
 
     #receive until there is no more output
     if p.can_recv(timeout):
@@ -74,8 +81,20 @@ def check_format(file, timeout=1):
         p.close()
 
 def main():
-    args = parser.parse_args()
 
+    print('                                _       _ _   ')
+    print('                               | |     (_) |  ')
+    print('  _ ____      ___ __  ___ _ __ | | ___  _| |_ ')
+    print(' | \'_ \\ \\ /\\ / / \'_ \\/ __| \'_ \\| |/ _ \\| | __|')
+    print(' | |_) \\ V  V /| | | \\__ \\ |_) | | (_) | | |_ ')
+    print(' | .__/ \\_/\\_/ |_| |_|___/ .__/|_|\\___/|_|\\__|')
+    print(' | |                     | |                  ')
+    print(' |_|                     |_|                  ')
+    print('By QHpix.')
+    args = parser.parse_args()
+    if not args.file and not args.host:
+        print('No method selected, please do pwnsploit.py -h for more info')
+        exit()
     if args.timeout == None:
         args.timeout = 1
 
@@ -83,11 +102,11 @@ def main():
         check_bof(args.file, args.timeout)
 
     elif args.format:
-        check_format(args.file, args.timeout)
+        check_format(args.file, args.timeout, args.host, args.port)
 
     else:
         check_bof(args.file, args.timeout)
-        check_format(args.file, args.timeout)
+        check_format(args.file, args.timeout, args.host, args.port)
 
 if __name__ == '__main__':
     main()
